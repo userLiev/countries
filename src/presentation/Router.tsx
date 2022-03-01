8ea95bb6db94227609676c0ac4fe6644a13aacc0
Gabriel Coelho <gabriellc2307@gmail.com>
2022-03-01 16:00:07 -0300

    feat: fallback route redirect

--- src/presentation/Router.tsx
+++ src/presentation/Router.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Layout from './components/layout/Layout';
@@ -16,6 +16,7 @@ const Router: FC = () => {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/info" element={<InfoPage />} />
+            <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </BrowserRouter>
       </Main>
